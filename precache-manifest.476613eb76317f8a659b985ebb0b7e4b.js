25080919e5a60587dccf832bb124afde2022795e
SUMIT <64258056+malisumit86@users.noreply.github.com>
2020-07-22 15:47:02 +0530

    Updates

---
+++ precache-manifest.476613eb76317f8a659b985ebb0b7e4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aee01fd30261c9533dfdaf8a11112ec",
+    "url": "/instaappclone/index.html"
+  },
+  {
+    "revision": "07d5571775075fba9d57",
+    "url": "/instaappclone/static/css/main.003e8689.chunk.css"
+  },
+  {
+    "revision": "a342476f0a8448265ea0",
+    "url": "/instaappclone/static/js/2.5b1c126c.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/instaappclone/static/js/2.5b1c126c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d5571775075fba9d57",
+    "url": "/instaappclone/static/js/main.33acf3bd.chunk.js"
+  },
+  {
+    "revision": "cbcaeb00e25af2620f07",
+    "url": "/instaappclone/static/js/runtime-main.554d1200.js"
+  }
+]);
